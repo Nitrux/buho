4c3468baeb7b8c6753cb16c6c5a2febbe9c9c2df
Uri Herrera <hello@uriherrera.com>
2021-11-12 02:43:17 -0600

    fix email

--- travis-build.sh
+++ travis-build.sh
@@ -102,7 +102,7 @@ checkinstall -D -y \
 	--pkggroup=lib \
 	--pkgsource=buho \
 	--pakdir=../.. \
-	--maintainer="Uri Herrera <uri_herrera@nxos.org>" \
+	--maintainer=uri_herrera@nxos.org \
 	--provides=buho \
 	--requires="libc6,libkf5coreaddons5,libkf5i18n5,libqt5core5a,libqt5gui5,libqt5network5,libqt5qml5,libqt5sql5,libqt5widgets5,libstdc++6,mauikit \(\>= 2.1.0\),mauikit-accounts \(\>= 2.1.0\),mauikit-filebrowsing \(\>= 2.1.0\),qml-module-qt-labs-platform,qml-module-qtwebview" \
 	--nodoc \
