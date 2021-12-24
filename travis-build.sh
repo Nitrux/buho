ea58da71ef3f6d22ac41fb8ca86c43174f2a65a8
UriHerrera <hello@uriherrera.com>
2021-12-24 14:09:12 -0600

    update pkg

--- travis-build.sh
+++ travis-build.sh
@@ -94,12 +94,12 @@ make
 checkinstall -D -y \
 	--install=no \
 	--fstrans=yes \
-	--pkgname=buho \
+	--pkgname=buho-git \
 	--pkgversion=2.1.1+git \
 	--pkgarch=amd64 \
 	--pkgrelease="1" \
 	--pkglicense=LGPL-3 \
-	--pkggroup=lib \
+	--pkggroup=utils \
 	--pkgsource=buho \
 	--pakdir=../.. \
 	--maintainer=uri_herrera@nxos.org \
