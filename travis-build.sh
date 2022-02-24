5dc9b78886cd3b2558ba562335079579c1500956
UriHerrera <hello@uriherrera.com>
2022-02-24 17:40:18 -0600

    update pkg

--- travis-build.sh
+++ travis-build.sh
@@ -100,7 +100,7 @@ checkinstall -D -y \
 	--install=no \
 	--fstrans=yes \
 	--pkgname=buho-git \
-	--pkgversion=2.1.1+git+1 \
+	--pkgversion=2.1.1+git+2 \
 	--pkgarch=amd64 \
 	--pkgrelease="1" \
 	--pkglicense=LGPL-3 \
