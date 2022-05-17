9cad99f4f312cc25f978c3a1be5e8f37a121fc6a
Uri Herrera <hello@uriherrera.com>
2022-05-16 19:28:01 -0500

    update pkg

--- travis-build.sh
+++ travis-build.sh
@@ -104,7 +104,7 @@ checkinstall -D -y \
 	--install=no \
 	--fstrans=yes \
 	--pkgname=buho-git \
-	--pkgversion=2.1.2+git+2 \
+	--pkgversion=2.1.2+git+3 \
 	--pkgarch=amd64 \
 	--pkgrelease="1" \
 	--pkglicense=LGPL-3 \
