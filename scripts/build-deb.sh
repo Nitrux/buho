ac693a6c99df11d30e1e4250846ca6666c42ed68
Uri Herrera <hello@uriherrera.com>
2022-07-19 21:22:02 -0500

    update stuff

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -1,77 +1,37 @@
-#!/bin/bash
+#! /bin/bash
 
 set -x
 
-### Install Build Tools #1
-
-DEBIAN_FRONTEND=noninteractive apt -qq update
-DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
-	appstream \
-	automake \
-	autotools-dev \
-	build-essential \
-	checkinstall \
-	cmake \
-	curl \
-	devscripts \
-	equivs \
-	extra-cmake-modules \
-	gettext \
-	git \
-	gnupg2 \
-	lintian \
-	wget
-
-### Add Neon Sources
+### Update sources
 
 wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.neon.user
+
+wget -qO /etc/apt/sources.list.d/nitrux-main-compat-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux
+
+wget -qO /etc/apt/sources.list.d/nitrux-testing-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux.testing
 
 DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
 	55751E5D > /dev/null
 
+curl -L https://packagecloud.io/nitrux/repo/gpgkey | apt-key add -;
+curl -L https://packagecloud.io/nitrux/compat/gpgkey | apt-key add -;
 curl -L https://packagecloud.io/nitrux/testing/gpgkey | apt-key add -;
-
-wget -qO /etc/apt/sources.list.d/nitrux-testing-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux.testing
 
 DEBIAN_FRONTEND=noninteractive apt -qq update
 
 ### Install Package Build Dependencies #2
 
 DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
-	libkf5config-dev \
-	libkf5coreaddons-dev \
-	libkf5i18n-dev \
-	libkf5kio-dev \
-	libkf5notifications-dev \
-	libkf5service-dev \
-	libqt5svg5-dev \
-	libqt5webview5-dev \
-	libwayland-dev \
-	mauikit-accounts-git \
-	mauikit-filebrowsing-git \
 	mauikit-git \
-	qtbase5-dev \
-	qtdeclarative5-dev \
-	qtquickcontrols2-5-dev \
-	qtwayland5 \
-	qtwayland5-dev-tools \
-	qtwayland5-private-dev \
-	qtwebengine5-dev
+	mauikit-filebrowsing-git
 
-### Clone repo.
+### Download Source
 
-git clone --single-branch --branch master https://invent.kde.org/maui/buho.git
-
-rm -rf buho/{android_files,macos_files,windows_files,examples,LICENSES,README.md}
-
-
-### Fix .desktop (values extending the format should start with "X-")
-
-sed -i 's+Categories=Qt;KDE;Utility;TextTools;TextEditor;Todo;X-KDE-Utilities-Desktop;+Categories=Qt;KDE;Utility;TextTools;TextEditor;X-ToDo;X-KDE-Utilities-Desktop;+g' buho/src/org.kde.buho.desktop
+git clone --depth 1 --branch $BUHO_BRANCH https://invent.kde.org/maui/buho.git
 
 ### Compile Source
 
-mkdir -p buho/build && cd buho/build
+mkdir -p build && cd build
 
 cmake \
 	-DCMAKE_INSTALL_PREFIX=/usr \
@@ -84,12 +44,11 @@ cmake \
 	-DCMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY=ON \
 	-DCMAKE_INSTALL_RUNSTATEDIR=/run "-GUnix Makefiles" \
 	-DCMAKE_VERBOSE_MAKEFILE=ON \
-	-DCMAKE_INSTALL_LIBDIR=lib/x86_64-linux-gnu ..
+	-DCMAKE_INSTALL_LIBDIR=lib/x86_64-linux-gnu ../buho/
 
-make
+make -j$(nproc)
 
 ### Run checkinstall and Build Debian Package
-### DO NOT USE debuild, screw it
 
 >> description-pak printf "%s\n" \
 	'MauiKit Note Taking App.' \
@@ -104,13 +63,13 @@ checkinstall -D -y \
 	--install=no \
 	--fstrans=yes \
 	--pkgname=buho-git \
-	--pkgversion=2.1.2+git+3 \
+	--pkgversion=$PACKAGE_VERSION \
 	--pkgarch=amd64 \
 	--pkgrelease="1" \
 	--pkglicense=LGPL-3 \
 	--pkggroup=utils \
 	--pkgsource=buho \
-	--pakdir=../.. \
+	--pakdir=. \
 	--maintainer=uri_herrera@nxos.org \
 	--provides=buho \
 	--requires="libc6,libkf5coreaddons5,libkf5i18n5,libqt5core5a,libqt5gui5,libqt5network5,libqt5qml5,libqt5sql5,libqt5widgets5,libstdc++6,mauikit-git \(\>= 2.1.2+git+1\),mauikit-accounts-git \(\>= 2.1.2+git+1\),mauikit-filebrowsing-git \(\>= 2.1.2+git+1\),qml-module-qt-labs-platform,qml-module-qtwebview" \
